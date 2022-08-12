51fcb2950d54495e51626d6b5e975e47924e6c48
unknown <evatrancho@gmail.com>
2022-08-12 11:48:05 -0700

    activities

---
+++ story_time.js
@@ -0,0 +1,31 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let name = 'Mercedes';
+let object = "lottery jackpot";
+let place = "a street corner";
+let place1 = "Alicante (Spain)";
+let place2 = "tobaco shop";
+let job = "shop owner";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let money = 1.27;
+let time = 9;
+let years = 5;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let objects = ['money', 'clothes', 'food'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let smile = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("It was just a normal day for " + name + " when she found out that she had won the ");
+console.log("US$" + money + " million " + object + ". Starting at " + time + "am everyday, she would stand on ");
+console.log(place + " for the next " + years + " years in " + place1 + " begging for " + objects + "..." + "\n");
+console.log("When she was done that day, " + name + " headed over to the" + place2 +"where she bought her lottery") 
+console.log("tickets every day and said to the " + job + " with a really " + smile + " big smile: ");
+console.log("'I think the numbers on TV are the same as the ones on my ticket!'");
